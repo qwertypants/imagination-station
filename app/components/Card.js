e258d524766d285379fee517cf3a97e0b6f7c4d5
Wilkins Fernandez <wilkinsf@gmail.com>
2024-08-07 16:50:36 -0400

    styles

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,18 +1,18 @@
 const Card = ({ image, content, tags }) => (
   <div className="mb-4 w-full overflow-hidden rounded-lg shadow-md">
     <img src={image} alt={content} className="w-full object-cover" />
-    <div className="p-4">
-      <p className="mb-2 text-gray-700">{content}</p>
-      <div className="flex flex-wrap gap-2">
+    <div className="px-4">
+      <div className="mt-4 flex flex-wrap gap-2">
         {tags.map((tag, index) => (
           <span
             key={index}
-            className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600"
+            className="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600"
           >
             {tag}
           </span>
         ))}
       </div>
+      <p className="mb-2 py-2 text-gray-700">{content}</p>
     </div>
   </div>
 );
